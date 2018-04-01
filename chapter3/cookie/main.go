f6b7cce2f417af21a5aaf1bd95dd8ca930fef1a8
ssmylh <ssmylh@gmail.com>
2018-04-01 21:12:14 +0900

    Chapter 3.10

---
+++ chapter3/cookie/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"net/http/cookiejar"
+	"net/http/httputil"
+	"net/url"
+)
+
+func main() {
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		panic(err)
+	}
+
+	client := http.Client{
+		Jar: jar,
+	}
+	for i := 0; i < 2; i++ {
+		resp, err := client.Get("http://localhost:18888/cookie")
+		if err != nil {
+			panic(err)
+		}
+		dump, err := httputil.DumpResponse(resp, true)
+		if err != nil {
+			panic(err)
+		}
+		log.Println(string(dump))
+	}
+
+	url, err := url.Parse("http://localhost")
+	if err != nil {
+		panic(err)
+	}
+	log.Println(jar.Cookies(url))
+}
